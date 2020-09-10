e8efa46d3482e4c9afbeb3cadf64d52d1f2e6545
dima <dimm1simm1@gmail.com>
2020-09-11 00:26:38 +0300

    sss

--- src/components/EnhancedTable.js
+++ src/components/EnhancedTable.js
@@ -19,7 +19,181 @@ import {
   useRowSelect,
   useSortBy,
   useTable,
+  useFilters, useAsyncDebounce
 } from 'react-table'
+import matchSorter from 'match-sorter'
+
+function GlobalFilter({
+                        preGlobalFilteredRows,
+                        globalFilter,
+                        setGlobalFilter,
+                      }) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  const onChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+      <span>
+      Search:{' '}
+        <input
+            value={value || ""}
+            onChange={e => {
+              setValue(e.target.value);
+              onChange(e.target.value);
+            }}
+            placeholder={`${count} records...`}
+            style={{
+              fontSize: '1.1rem',
+              border: '0',
+            }}
+        />
+    </span>
+  )
+}
+
+
+function DefaultColumnFilter({
+                               column: { filterValue, preFilteredRows, setFilter },
+                             }) {
+  const count = preFilteredRows.length
+
+  return (
+      <input
+          value={filterValue || ''}
+          onChange={e => {
+            setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+          }}
+          placeholder={`Search ${count} records...`}
+      />
+  )
+}
+
+function SelectColumnFilter({
+                              column: { filterValue, setFilter, preFilteredRows, id },
+                            }) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach(row => {
+      options.add(row.values[id])
+    })
+    return [...options.values()]
+  }, [id, preFilteredRows])
+
+  // Render a multi-select box
+  return (
+      <select
+          value={filterValue}
+          onChange={e => {
+            setFilter(e.target.value || undefined)
+          }}
+      >
+        <option value="">All</option>
+        {options.map((option, i) => (
+            <option key={i} value={option}>
+              {option}
+            </option>
+        ))}
+      </select>
+  )
+}
+
+function SliderColumnFilter({
+                              column: { filterValue, setFilter, preFilteredRows, id },
+                            }) {
+  // Calculate the min and max
+  // using the preFilteredRows
+
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    preFilteredRows.forEach(row => {
+      min = Math.min(row.values[id], min)
+      max = Math.max(row.values[id], max)
+    })
+    return [min, max]
+  }, [id, preFilteredRows])
+
+  return (
+      <>
+        <input
+            type="range"
+            min={min}
+            max={max}
+            value={filterValue || min}
+            onChange={e => {
+              setFilter(parseInt(e.target.value, 10))
+            }}
+        />
+        <button onClick={() => setFilter(undefined)}>Off</button>
+      </>
+  )
+}
+
+function NumberRangeColumnFilter({
+                                   column: { filterValue = [], preFilteredRows, setFilter, id },
+                                 }) {
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    preFilteredRows.forEach(row => {
+      min = Math.min(row.values[id], min)
+      max = Math.max(row.values[id], max)
+    })
+    return [min, max]
+  }, [id, preFilteredRows])
+
+  return (
+      <div
+          style={{
+            display: 'flex',
+          }}
+      >
+        <input
+            value={filterValue[0] || ''}
+            type="number"
+            onChange={e => {
+              const val = e.target.value
+              setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]])
+            }}
+            placeholder={`Min (${min})`}
+            style={{
+              width: '70px',
+              marginRight: '0.5rem',
+            }}
+        />
+        to
+        <input
+            value={filterValue[1] || ''}
+            type="number"
+            onChange={e => {
+              const val = e.target.value
+              setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined])
+            }}
+            placeholder={`Max (${max})`}
+            style={{
+              width: '70px',
+              marginLeft: '0.5rem',
+            }}
+        />
+      </div>
+  )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+
+
+
+
 
 const IndeterminateCheckbox = React.forwardRef(
   ({ indeterminate, ...rest }, ref) => {
@@ -69,6 +243,7 @@ const EditableCell = ({
     setValue(initialValue)
   }, [initialValue])
 
+
   return (
     <input
       style={inputStyle}
@@ -104,6 +279,25 @@ const EnhancedTable = ({
   updateMyData,
   skipPageReset,
 }) => {
+    const filterTypes = React.useMemo(
+        () => ({
+            // Add a new fuzzyTextFilterFn filter type.
+            fuzzyText: fuzzyTextFilterFn,
+            // Or, override the default text filter to use
+            // "startWith"
+            text: (rows, id, filterValue) => {
+                return rows.filter(row => {
+                    const rowValue = row.values[id]
+                    return rowValue !== undefined
+                        ? String(rowValue)
+                            .toLowerCase()
+                            .startsWith(String(filterValue).toLowerCase())
+                        : true
+                })
+            },
+        }),
+        []
+    )
   const {
     getTableProps,
     headerGroups,
@@ -113,13 +307,19 @@ const EnhancedTable = ({
     setPageSize,
     preGlobalFilteredRows,
     setGlobalFilter,
+      getTableBodyProps,
+      rows,
+      state,
+      visibleColumns,
     state: { pageIndex, pageSize, selectedRowIds, globalFilter },
   } = useTable(
     {
       columns,
       data,
       defaultColumn,
+      filterTypes,
       autoResetPage: !skipPageReset,
+      useFilters, // useFilters!
       // updateMyData isn't part of the API, but
       // anything we put into these options will
       // automatically be available on the instance.
@@ -127,6 +327,7 @@ const EnhancedTable = ({
       // cell renderer!
       updateMyData,
     },
+      useFilters, // useFilters!
     useGlobalFilter,
     useSortBy,
     usePagination,
@@ -159,7 +360,7 @@ const EnhancedTable = ({
       ])
     }
   )
-
+    const firstPageRows = rows.slice(0, 10)
   const handleChangePage = (event, newPage) => {
     gotoPage(newPage)
   }
@@ -213,6 +414,9 @@ const EnhancedTable = ({
                       direction={column.isSortedDesc ? 'desc' : 'asc'}
                     />
                   ) : null}
+
+                        <div>{column.canFilter ? column.render('Filter') : null}</div>
+
                 </TableCell>
               ))}
             </TableRow>
